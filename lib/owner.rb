37e5e39156b525888fb3c1a49afd94c89c5e597a
Stephen Wright <scw248@gmail.com>
2019-01-29 15:41:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,4 +40,15 @@ class Owner
     #binding.pry
   end
   
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+    #binding.pry
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+    #binding.pry
+  end
 end
